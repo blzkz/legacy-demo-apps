471f7b702f531c72266bab9528853076d99614e3
jscanzoni <jason@scnz.co>
2021-10-08 00:06:03 -0400

    another update

--- generator/template/data/reset.php
+++ generator/template/data/reset.php
@@ -1,6 +1,6 @@
 <?php
 if (file_exists("./config.json"))
-    $config = json_decode(file_get_contents('./data/config.json'), true);
+    $config = json_decode(file_get_contents('./config.json'), true);
 else
     die("Can't find config.json");
 
