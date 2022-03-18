33508138d9885936c82599a469ffa1888bf95cee
Jason Scanzoni <jason@scnz.co>
2022-03-18 15:18:21 -0400

    IDs for page total

--- generator/template/includes/data_summary.php
+++ generator/template/includes/data_summary.php
@@ -63,7 +63,7 @@ if ($total <= $end) {
     }
     echo "</table>";
     echo "<br><br>";
-    echo "$start - $end of $total<br><br>";
+    echo "<span id=\"pageStart\">$start</span> - <span id=\"pageEnd\">$end</span> of <span id=\"totalRecords\">$total<span><br><br>";
 
     if (!$lastpage)
         echo "<a href=\"list.php?s=" . ($end + 1) . "\">Next Page</a><br>";
